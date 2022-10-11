16a78e9892e14a1e5cb7e82425eae34b84eaab0d
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2022-10-11 00:02:51 -0300

    correcao 05

--- 05/scripts.js
+++ 05/scripts.js
@@ -1,17 +1,24 @@
 const pacientes = ["José", "Pedro", "Maria", "João", "Ana", "Bárbara", "Joana"];
 
 const agendarAtendimento = (pacientes, nome) => {
+  let index = pacientes.indexOf(nome);
+  pacientes.splice(index, 1);
   pacientes.push(nome);
+  return pacientes.join(", ");
+};
+
+const atenderPaciente = (pacientes, nome) => {
+  let index = pacientes.indexOf(nome);
+  pacientes.splice(index, 1);
   return pacientes.join(", ");
 };
 
 const cancelarAtendimento = (pacientes, nome) => {
   const posicao = pacientes.indexOf(nome);
-
   pacientes.splice(posicao, 1);
-
   return pacientes.join(", ");
 };
 
-console.log(agendarAtendimento(pacientes, "Power Rangers"));
+console.log(agendarAtendimento(pacientes, "Pedro"));
 console.log(cancelarAtendimento(pacientes, "Ana"));
+console.log(atenderPaciente(pacientes, "José"));
